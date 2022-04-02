d9e32f83f6cb8432136e9c70127a75a02a27bb93
igor-minaev <igor.minayeu@gmail.com>
2022-04-03 00:06:17 +0300

    add: Escape Sequences in Strings

--- script.js
+++ script.js
@@ -150,3 +150,7 @@ const myStr = 'I am a "double quoted" string inside "double quotes".'; // Change
 /* Quoting Strings with Single Quotes */
 
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+/* Escape Sequences in Strings */
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
