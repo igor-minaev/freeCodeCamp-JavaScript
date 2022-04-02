c4d43fca6004b741e3bc88403a72c10d342e4c5f
igor-minaev <igor.minayeu@gmail.com>
2022-04-02 14:04:39 +0300

    add: Understanding Uninitialized Variables

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ var a = 9;
 
 var myFirstName = "Igor";
 var myLastName = "Minaev";
+
+/* Understanding Uninitialized Variables */
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
