8c02c535ec4168a65c506802f82acab08b8923df
igor-minaev <igor.minayeu@gmail.com>
2022-04-02 15:27:55 +0300

    add: Declare a Read-Only Variable with the const Keyword

--- script.js
+++ script.js
@@ -42,3 +42,10 @@ titleCaseOver = 9000;
 
 let camper = "James";
 let camper = "David";
+
+/* Declare a Read-Only Variable with the const Keyword */
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
