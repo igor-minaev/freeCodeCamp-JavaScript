ea34fd426accc08a980510ec6693dec88be3b667
igor-minaev <igor.minayeu@gmail.com>
2022-04-03 13:54:46 +0300

    add: Use Bracket Notation to Find the Nth-to-Last Character in a String

--- script.js
+++ script.js
@@ -220,3 +220,11 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+/* Use Bracket Notation to Find the Nth-to-Last Character in a String */
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
