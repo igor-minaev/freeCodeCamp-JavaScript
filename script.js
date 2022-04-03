7fee55a12c01d3a0ab75600dd0f32b28a9fd9d8d
igor-minaev <igor.minayeu@gmail.com>
2022-04-03 14:16:31 +0300

    add: Modify Array Data With Indexes

--- script.js
+++ script.js
@@ -265,3 +265,11 @@ const myArray = [
 
 const myArray = [50, 60, 70];
 const myData = myArray[0];
+
+/* Modify Array Data With Indexes */
+
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0] = 45;
