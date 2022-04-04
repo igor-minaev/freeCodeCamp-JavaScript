0e5d4b758478ec3f7eca247eda15276193a80e96
igor-minaev <igor.minayeu@gmail.com>
2022-04-04 21:32:36 +0300

    add: Comparisons with the Logical Or Operator

--- script.js
+++ script.js
@@ -635,3 +635,18 @@ function testLogicalAnd(val) {
 }
 
 testLogicalAnd(10);
+
+/* Comparisons with the Logical Or Operator */
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
