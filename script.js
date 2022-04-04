ece8ed2a608fc25f8c8351d1ff96b987616e53a3
igor-minaev <igor.minayeu@gmail.com>
2022-04-04 17:27:43 +0300

    add: Use Conditional Logic with If Statements

--- script.js
+++ script.js
@@ -471,3 +471,15 @@ function welcomeToBooleans() {
 
   // Only change code above this line
 }
+
+/* Use Conditional Logic with If Statements */
+
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+  if (wasThatTrue) {
+    return "Yes, that was true";
+  }
+  return "No, that was false";
+
+  // Only change code above this line
+}
