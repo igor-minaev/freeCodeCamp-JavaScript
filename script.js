8ed81eee56bffc84ea8751df28dbe1e8108edbb9
igor-minaev <igor.minayeu@gmail.com>
2022-04-04 17:31:08 +0300

    add: Comparison with the Equality Operator

--- script.js
+++ script.js
@@ -483,3 +483,16 @@ function trueOrFalse(wasThatTrue) {
 
   // Only change code above this line
 }
+
+/* Comparison with the Equality Operator */
+
+// Setup
+function testEqual(val) {
+  if (val == 12) {
+    // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
