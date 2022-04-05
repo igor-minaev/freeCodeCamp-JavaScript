58f642eddfd042dbe6cc389de566128deb99c636
igor-minaev <igor.minayeu@gmail.com>
2022-04-05 22:41:37 +0300

    add: Delete Properties from a JavaScript Object

--- script.js
+++ script.js
@@ -1006,3 +1006,17 @@ const myDog = {
 };
 
 myDog["bark"] = "woof";
+
+/* Delete Properties from a JavaScript Object */
+
+// Setup
+const myDog = {
+  name: "Happy Coder",
+  legs: 4,
+  tails: 1,
+  friends: ["freeCodeCamp Campers"],
+  bark: "woof",
+};
+
+delete myDog.tails;
+// Only change code below this line
