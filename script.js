f8af1c158eba7df9c3decca0c36e4d248ab20b3e
igor-minaev <igor.minayeu@gmail.com>
2022-04-05 22:29:08 +0300

    add: Accessing Object Properties with Variables

--- script.js
+++ script.js
@@ -955,3 +955,29 @@ const testObj = {
 // Only change code below this line
 const hatValue = testObj.hat; // Change this line
 const shirtValue = testObj.shirt; // Change this line
+
+/* Accessing Object Properties with Bracket Notation */
+
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water",
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"]; // Change this line
+const drinkValue = testObj["the drink"]; // Change this line
+
+/* Accessing Object Properties with Variables */
+
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas",
+};
+
+// Only change code below this line
+const playerNumber = 16; // Change this line
+const player = testObj[playerNumber]; // Change this line
