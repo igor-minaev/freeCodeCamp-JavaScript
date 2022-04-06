214460ac4ccd6341d89073a6b464932d01748c6c
igor-minaev <igor.minayeu@gmail.com>
2022-04-06 21:40:31 +0300

    add: Accessing Nested Arrays

--- script.js
+++ script.js
@@ -1085,3 +1085,18 @@ const myStorage = {
 };
 
 const gloveBoxContents = myStorage.car.inside["glove box"];
+
+/* Accessing Nested Arrays */
+
+const myPlants = [
+  {
+    type: "flowers",
+    list: ["rose", "tulip", "dandelion"],
+  },
+  {
+    type: "trees",
+    list: ["fir", "pine", "birch"],
+  },
+];
+
+const secondTree = myPlants[1].list[1];
