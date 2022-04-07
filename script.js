4ad0d2b13fd0862a2b76c4c32c1b92504e3c26ab
igor-minaev <igor.minayeu@gmail.com>
2022-04-07 19:41:36 +0300

    add: Replace Loops using Recursion

--- script.js
+++ script.js
@@ -1222,3 +1222,15 @@ do {
   myArray.push(i);
   i++;
 } while (i < 5);
+
+/* Replace Loops using Recursion */
+
+function sum(arr, n) {
+  // Only change code below this line
+  if (n === 0) {
+    return 0;
+  } else {
+    return sum(arr, n - 1) + arr[n - 1];
+  }
+}
+// Only change code above this line
