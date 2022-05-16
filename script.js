704275e83db4be5d57713d042f5e9ee8d80179aa
igor-minaev <igor.minayeu@gmail.com>
2022-05-16 23:24:18 +0300

    fix: Use the parseInt Function with a Radix

--- script.js
+++ script.js
@@ -1319,7 +1319,7 @@ convertToInteger("56");
 //Use the parseInt Function with a Radix
 
 function convertToInteger(str) {
-  return parseInt(str, 10);
+  return parseInt(str, 2);
 }
 
 convertToInteger("10011");
