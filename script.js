c66a4f811a6d7c0e5193f77690bed6939930b622
igor-minaev <igor.minayeu@gmail.com>
2022-05-16 23:17:32 +0300

    add: Generate Random Whole Numbers within a Range

--- script.js
+++ script.js
@@ -1299,3 +1299,11 @@ function randomWholeNum() {
 
   return Math.floor(Math.random() * 10);
 }
+
+//Generate Random Whole Numbers within a Range
+
+function randomRange(myMin, myMax) {
+  // Only change code below this line
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+  // Only change code above this line
+}
