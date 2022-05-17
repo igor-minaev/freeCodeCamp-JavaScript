a28147b911bf73fee65548fc5c353e20fa399809
igor-minaev <igor.minayeu@gmail.com>
2022-05-17 18:41:21 +0300

    add: Use Recursion to Create a Countdown

--- script.js
+++ script.js
@@ -1339,3 +1339,17 @@ function checkSign(num) {
 }
 
 checkSign(10);
+
+//Use Recursion to Create a Countdown
+
+// Only change code below this line
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countdown(n - 1);
+    countArray.unshift(n);
+    return countArray;
+  }
+}
+// Only change code above this line
