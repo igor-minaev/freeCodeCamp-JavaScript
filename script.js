681c1f8ffb1e5b8afd3495802ac364295991e33d
igor-minaev <igor.minayeu@gmail.com>
2022-05-17 18:10:59 +0300

    add: Use the Conditional (Ternary) Operator

--- script.js
+++ script.js
@@ -1323,3 +1323,11 @@ function convertToInteger(str) {
 }
 
 convertToInteger("10011");
+
+//Use the Conditional (Ternary) Operator
+
+function checkEqual(a, b) {
+  return a === b ? "Equal" : "Not Equal";
+}
+
+checkEqual(1, 2);
