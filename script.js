c16f53bd8ebf9b637e14c2c730e692c2e73c5825
Igor Minayeu <igor.minayeu@gmail.com>
2022-06-20 22:23:10 +0300

    add: Write Concise Declarative Functions with ES6

--- script.js
+++ script.js
@@ -1554,3 +1554,16 @@ const createPerson = (name, age, gender) => {
   };
   // Only change code above this line
 };
+
+//Write Concise Declarative Functions with ES6
+
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
